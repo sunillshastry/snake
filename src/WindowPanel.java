7bbe1b3c661b3938d73875f64dadda2f7877ff8b
Sunil Shastry <sunil.qutrix@gmail.com>
2021-12-12 19:32:54 +0530

    Added gitignore file. Implemented basic class structure for three Java files

---
+++ src/WindowPanel.java
@@ -0,0 +1,8 @@
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class WindowPanel extends JPanel implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent event) {}
+}
