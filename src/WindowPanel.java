4df8cc4eb0d2585f90e9d334b3b3735a76026f10
Sunil Shastry <sunil.shastry69@gmail.com>
2021-12-12 19:47:34 +0530

    Added import statements and method prototypes for WindowPanel

--- src/WindowPanel.java
+++ src/WindowPanel.java
@@ -1,9 +1,32 @@
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Graphics;
 
 public class WindowPanel extends JPanel implements ActionListener {
     public WindowPanel() {}
+
+    public void startGame() {}
+
+    public void paintComponent(Graphics g) {}
+
+    public void draw(Graphics g) {}
+
+    public void move() {}
+
+    public void checkEnergy() {}
+
+    public void checkCollision() {}
+
+    public void gameOver(Graphics g) {}
+
     @Override
     public void actionPerformed(ActionEvent event) {}
+
+    public class CustomKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent event) {}
+    }
 }
